0bd546de3e297563e3182bedf9a6e602a87d420b
Carlos <arq.camejia@gmail.com>
2020-12-14 12:14:53 -0400

    Inital commit, before adding vue-router

---
+++ src/views/ContactoInfo.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<h1>Hello from Contacto e info</h1>
+	</div>
+</template>
+<script>
+export default {
+	name: 'ContactoInfo',
+};
+</script>
---
+++ src/views/Galeria.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<h1>Hello from Galeria</h1>
+	</div>
+</template>
+<script>
+export default {
+	name: 'Galeria',
+};
+</script>
